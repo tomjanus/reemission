beea89eca00068e845dff57ee95601bf7958207c
tjanus <tomasz.k.janus@gmail.com>
2021-12-21 23:16:18 +0100

    minor formatting change

--- src/emissions/temperature.py
+++ src/emissions/temperature.py
@@ -17,8 +17,8 @@ class MonthlyTemperature:
     def eff_temp(self, coeff: float = 0.052) -> float:
         """ Calculate effective annual Temperature CO2 in deg C for the
             estimation of CO2 and CH4 diffusion)
-            - Currently the coefficient used in CO2 calcs is 0.05 whilst for CH4
-              it is 0.052
+            - Currently the coefficient used in CO2 calcs is 0.05 whilst for
+              CH4 it is 0.052
         """
         return math.log10(
             mean([10**(temp * coeff) for temp in self.temp_profile]))/coeff
