fc60b9b152faee2ab6a8643bba9a6757fd426864
tjanus <tomasz.k.janus@gmail.com>
2021-12-30 03:50:18 +0100

    add config enabled batch calculation of gas emissions

--- src/emissions/model.py
+++ src/emissions/model.py
@@ -1,7 +1,9 @@
 """ Module with simulation/calculation facilities for calculating GHG
     emissions for man-made reservoirs """
 from dataclasses import dataclass, field
-from typing import Type
+from typing import Type, Dict, List, Tuple, Union, Optional
+from itertools import chain
+import yaml
 from .input import Inputs
 from .temperature import MonthlyTemperature
 from .catchment import Catchment
@@ -9,73 +11,126 @@ from .reservoir import Reservoir
 from .emissions import (CarbonDioxideEmission,
                         NitrousOxideEmission,
                         MethaneEmission)
+from .presenter import Presenter
+
+# TODO: potential problems in Python <= 3.6 because dicts are not ordered
+#       pay attention
+truths = [True, 'true', 'True', 'yes', 'on']
+falses = [False, 'false', 'False', 'no', 'off', 'null']
 
 
 @dataclass
 class EmissionModel:
     """ Calculates emissions for a set of data provided in a dictionary
         format """
-    outputs: float = field(init=False)
+    config: Union[Dict, str]
+    presenters: Optional[List[Type[Presenter]]] = None
+    outputs: Dict = field(init=False)
 
-    def calculate(self, inputs: Type[Inputs]) -> None:
+    def __post_init__(self):
+        self.outputs = {}
+        if isinstance(self.config, str):
+            with open(self.config) as file:
+                self.config = yaml.load(file, Loader=yaml.FullLoader)
+
+    @staticmethod
+    def create_exec_dictionary(co2_em: Union[CarbonDioxideEmission, None],
+                               ch4_em: Union[MethaneEmission, None],
+                               n2o_em: Union[NitrousOxideEmission, None],
+                               years: Tuple[int] =
+                               (1, 5, 10, 20, 30, 40, 50, 100)) -> Dict:
+        """ Create a dictionary with function references and arguments for
+            calculating gas emissions """
+        co2_exec, ch4_exec, n2o_exec = {}, {}, {}
+        if co2_em:
+            co2_exec = {
+                'co2_diffusion': {'ref': co2_em.gross_total,
+                                  'args': {}},
+                'co2_diffusion_nonanthro': {'ref': co2_em.flux_nonanthro,
+                                            'args': {}},
+                'co2_preimp': {'ref': co2_em.pre_impoundment,
+                               'args': {}},
+                'co2_minus_nonanthro': {'ref': co2_em.net_total,
+                                        'args': {}},
+                'co2_net': {'ref': co2_em.factor,
+                            'args': {'number_of_years': years[-1]}},
+                'co2_profile': {'ref': co2_em.profile,
+                                'args': {'years': years}}
+            }
+        if ch4_em:
+            ch4_exec = {
+                'ch4_diffusion': {'ref': ch4_em.diffusion,
+                                  'args': {'number_of_years': years[-1]}},
+                'ch4_ebullition': {'ref': ch4_em.ebullition,
+                                   'args': {}},
+                'ch4_degassing': {'ref': ch4_em.degassing,
+                                  'args': {'number_of_years': years[-1]}},
+                'ch4_preimp': {'ref': ch4_em.pre_impoundment,
+                               'args': {}},
+                'ch4_net': {'ref': ch4_em.factor,
+                            'args': {'number_of_years': years[-1]}},
+                'ch4_profile': {'ref': ch4_em.profile,
+                                'args': {'years': years}}
+            }
+        if n2o_em:
+            n2o_exec = {
+                'n2o_methodA': {'ref': n2o_em.factor,
+                                'args': {'model': 'model 1'}},
+                'n2o_methodB': {'ref': n2o_em.factor,
+                                'args': {'model': 'model 2'}},
+                'n2o_mean': {'ref': n2o_em.factor,
+                             'args': {'mean': True}},
+                'n2o_profile': {'ref': n2o_em.profile,
+                                'args': {'years': years}},
+                }
+        exec_dict = dict(chain.from_iterable(
+            d.items() for d in (co2_exec, ch4_exec, n2o_exec)))
+        return exec_dict
+
+    def add_presenter(self, presenter: Type[Presenter]) -> None:
+        """ Add presenters to the emission model for data output formatting """
+        try:
+            self.presenters.append(presenter)
+        except AttributeError:
+            self.presenters = []
+            self.presenters.append(presenter)
+
+    def save_results(self):
+        """ Save results using presenters defined in the presenters list """
+
+    def calculate(self, inputs: Type[Inputs], p_calc_method='g-res',
+                  n2o_model='model 1') -> None:
         """ Calculate emissions for a number of variables defined in config """
+        # Iterate through each set of inputs and output results in a dict
         for model_input in inputs.inputs:
             monthly_temp = MonthlyTemperature(model_input.monthly_temps)
-            reservoir_data = model_input.reservoir_data
-            catchment_data = model_input.catchment_data
-            emission_factors = model_input.emission_factors
-            year_vector = model_input.year_vector
-            catchment = Catchment(**catchment_data)
-            reservoir = Reservoir(**reservoir_data,
+            # Instantiate Catchment and Reservoir objects
+            catchment = Catchment(**model_input.catchment_data)
+            reservoir = Reservoir(**model_input.reservoir_data,
                                   inflow_rate=catchment.discharge)
-            if "co2" in emission_factors:
+            # Instantiate emission objects for every gas to be computed
+            em_co2, em_n2o, em_ch4 = None, None, None
+            if "co2" in model_input.gasses:
                 em_co2 = CarbonDioxideEmission(
                     catchment=catchment, reservoir=reservoir,
                     eff_temp=monthly_temp.eff_temp(),
-                    p_calc_method='g-res')
-                co2_emission_profile = em_co2.profile(years=year_vector)
-                co2_emission_factor = em_co2.factor(
-                    number_of_years=year_vector[-1])
-                print("\n")
-                print(model_input.name)
-                print("CO2 emissions:")
-                print("---------------------------------------------")
-                print("CO2 emission profile: ",
-                      ["%.2f" % emission for emission in co2_emission_profile])
-                print("CO2 emission factor: %.2f" % co2_emission_factor)
-                print("\n")
-            if "n2o" in emission_factors:
-                em_n2o = NitrousOxideEmission(
-                    catchment=catchment, reservoir=reservoir, model='model 1')
-                n2o_emission_profile = em_n2o.profile(years=year_vector)
-                n2o_emission_factor = em_n2o.factor()
-                print(model_input.name)
-                print("N2O emissions:")
-                print("---------------------------------------------")
-                print("N2O emission profile: ", ["%.2f" % emission for emission in
-                                                 n2o_emission_profile])
-                print("N2O emission factor: %.2f" % n2o_emission_factor)
-                print("\n")
-                print('Calculate N concentration downstream of the reservoir')
-                print("---------------------------------------------")
-                tn_downstream_load = em_n2o.nitrogen_downstream_load()
-                tn_downstream_conc = em_n2o.nitrogen_downstream_conc()
-                print('TN downstream load (kg N yr-1): ', "%.1f" %
-                      tn_downstream_load)
-                print('TN downstream concentration (mg / L): ', "%.4f" %
-                      tn_downstream_conc)
-                print("\n")
-            if "ch4" in emission_factors:
+                    p_calc_method=p_calc_method)
+            if "ch4" in model_input.gasses:
                 em_ch4 = MethaneEmission(
                     catchment=catchment, reservoir=reservoir,
                     monthly_temp=monthly_temp, mean_ir=4.46)
-                ch4_emission_factor = em_ch4.factor()
-                ch4_emission_profile = em_ch4.profile(years=year_vector)
-                print(model_input.name)
-                print("CH4 emissions:")
-                print("---------------------------------------------")
-                print("CH4 emission profile: ",
-                      ["%.2f" % emission for emission in
-                       ch4_emission_profile])
-                print("CH4 emission factor: %.4f" % ch4_emission_factor)
-                print("\n")
+            if "n2o" in model_input.gasses:
+                em_n2o = NitrousOxideEmission(
+                    catchment=catchment, reservoir=reservoir, model=n2o_model)
+
+            exec_dict = self.create_exec_dictionary(
+                co2_em=em_co2, ch4_em=em_ch4, n2o_em=em_n2o,
+                years=model_input.year_vector)
+
+            output = {}
+            for gas in model_input.gasses:
+                for emission, em_config in self.config['outputs'][gas].items():
+                    if em_config['include'] in truths:
+                        output[emission] = exec_dict[emission]['ref'](
+                            **exec_dict[emission]['args'])
+            self.outputs[model_input.name] = output
