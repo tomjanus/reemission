196c654055617e6d2684e225a9eb2a9769822875
tjanus <tomasz.k.janus@gmail.com>
2022-01-06 02:39:38 +0100

    modify inputs variable type from list to dict and add error handling

--- src/emissions/input.py
+++ src/emissions/input.py
@@ -7,6 +7,7 @@ import logging
 from .biogenic import BiogenicFactors
 
 # Set up module logger
+logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 # Load path to Yaml tables
 module_dir = os.path.dirname(__file__)
@@ -16,17 +17,17 @@ module_dir = os.path.dirname(__file__)
 class Input:
     """ Input data for a single reservoir emission calculations """
     name: str
-    input_data: Dict
+    data: Dict
 
     @property
     def reservoir_data(self) -> Dict:
         """ Retrieve input data for reservoir-scale process calculations """
-        return self.input_data.get('reservoir')
+        return self.data.get('reservoir')
 
     @property
     def catchment_data(self) -> Dict:
         """ Retrieve input data for catchment-scale process calculations """
-        catchment_dict = self.input_data.get('catchment')
+        catchment_dict = self.data.get('catchment')
         catchment_dict["biogenic_factors"] = \
             BiogenicFactors.fromdict(catchment_dict["biogenic_factors"])
         return catchment_dict
@@ -34,50 +35,57 @@ class Input:
     @property
     def gasses(self) -> List[str]:
         """ Retrieve a list of emission factors to be calculated """
-        return self.input_data.get('gasses')
+        return self.data.get('gasses')
 
     @property
     def year_vector(self) -> Tuple[float]:
         """ Retrieve a tuple of years for which emissions profiles are
             being calculated """
-        return tuple(self.input_data.get('year_vector'))
+        return tuple(self.data.get('year_vector'))
 
     @property
     def monthly_temps(self) -> List[float]:
         """ Retrieve a vecor of monthly average temperatures """
-        return self.input_data.get('monthly_temps')
+        return self.data.get('monthly_temps')
 
     @classmethod
     def fromfile(cls, file: str, reservoir_name: str):
         """ Load inputs dictionary from json file """
         with open(file) as json_file:
             output_dict = json.load(json_file)
-            input_data = output_dict.get(reservoir_name)
-            if input_data is None:
+            data = output_dict.get(reservoir_name)
+            if data is None:
                 log.error("Reservoir '%s' not found. Returning empty class",
                           reservoir_name)
-        return cls(name=reservoir_name, input_data=input_data)
+        return cls(name=reservoir_name, data=data)
 
 
 @dataclass
 class Inputs:
     """ Collection of inputs for which GHG emissions are being calculated """
-    inputs: List[Input]
+    inputs: Dict[str, Input]
 
     def add_input(self, input_dict: Dict) -> None:
         """ Add new input dictionary into dictionary of inputs """
         reservoir_name = list(input_dict.keys())[0]
         input_data = input_dict.get(reservoir_name)
-        new_input = Input(name=reservoir_name, input_data=input_data)
-        self.inputs.append(new_input)
+        new_input = Input(name=reservoir_name, data=input_data)
+        if reservoir_name not in self.inputs:
+            self.inputs[reservoir_name] = new_input
+        else:
+            log.info("Key %s already in the inputs. Skipping", reservoir_name)
 
     @classmethod
     def fromfile(cls, file: str):
         """ Load inputs dictionary from json file """
-        inputs = []
+        inputs = {}
         with open(file) as json_file:
             output_dict = json.load(json_file)
             for reservoir_name, input_data in output_dict.items():
-                new_input = Input(name=reservoir_name, input_data=input_data)
-                inputs.append(new_input)
+                new_input = Input(name=reservoir_name, data=input_data)
+                if reservoir_name not in inputs:
+                    inputs[reservoir_name] = new_input
+                else:
+                    log.info("Key %s already in the inputs. Skipping",
+                             reservoir_name)
         return cls(inputs=inputs)
