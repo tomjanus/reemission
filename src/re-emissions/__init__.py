bf1a1959c0a450f6c2179644662f3c06196538df
tjanus <tomasz.k.janus@gmail.com>
2022-03-16 10:08:54 +0100

    change package name from emissions to re-emissions

---
+++ src/re-emissions/__init__.py
@@ -0,0 +1,8 @@
+""" """
+from pkg_resources import get_distribution, DistributionNotFound
+# automatically set __version__ to the global version of the package declared
+# in setup.cfg or set by the setuptools_scm package
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    pass  # package is not installed
