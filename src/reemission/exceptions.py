64d5947048ee6b01ba17388f4441836bcceb4cd4
tjanus <tomasz.k.janus@gmail.com>
2022-06-20 16:49:33 +0200

    (feat): Add exception to handle events when gas type is unknown.

--- src/reemission/exceptions.py
+++ src/reemission/exceptions.py
@@ -1,4 +1,4 @@
-""" Module containing custom exceptions """
+"""Module containing custom exceptions."""
 from typing import Tuple
 
 
@@ -7,7 +7,7 @@ class WrongN2OModelError(Exception):
        of unknown name/type.
 
     Attributes:
-        available_models: tuple with names of recognized N2O emission models.
+        permitted_models: tuple with names of recognized N2O emission models.
         message: explanation of the error
     """
     def __init__(
@@ -15,4 +15,23 @@ class WrongN2OModelError(Exception):
             permitted_models: Tuple[str, ...],
             message: str = "Model not recognized."):
         self.permitted_models = permitted_models
+        self.message = message + \
+            f"Permitted models: {self.permitted_models}"
+        super().__init__(self.message)
+
+
+class GasNotRecognizedException(Exception):
+    """Exception raised if attempting to select gas of unknown name/type.
+
+    Attributes:
+        permitted_gases: tuple with names of recognized emission gases.
+        message: explanation of the error
+    """
+    def __init__(
+            self,
+            permitted_gases: Tuple[str, ...],
+            message: str = "Gas not recognized."):
+        self.permitted_gases = permitted_gases
+        self.message = message + \
+            f"Permitted gases: {self.permitted_gases}"
         super().__init__(self.message)
