f6828dfb3a56cc065a973ca48ddddade2f9f5e80
tjanus <tomasz.k.janus@gmail.com>
2024-07-12 15:25:57 +0200

    docs: Update code documentation in exceptions.py

--- src/reemission/exceptions.py
+++ src/reemission/exceptions.py
@@ -1,19 +1,28 @@
-"""Module containing custom exceptions."""
+"""
+Module containing custom exceptions.
+"""
 from typing import Tuple, Union, Iterable
 
 
 class WrongN2OModelError(Exception):
-    """Exception raised if attempting to calculate with N2O emission model
-       of unknown name/type.
+    """Exception raised if attempting to calculate with N$_2$O emission model
+    of unknown name/type.
 
     Attributes:
-        permitted_models: tuple with names of recognized N2O emission models.
-        message: explanation of the error
+        permitted_models (Tuple[str, ...]): Names of recognized N$_2$O emission models.
+        message (str): Explanation of the error.
     """
     def __init__(
             self,
             permitted_models: Tuple[str, ...],
             message: str = "Model not recognized."):
+        """
+        Initializes the WrongN2OModelError with permitted models and an optional message.
+
+        Args:
+            permitted_models (Tuple[str, ...]): Permitted N$_2$O emission models.
+            message (str, optional): Additional message to include in the exception. Defaults to "Model not recognized.".
+        """
         self.permitted_models = permitted_models
         self.message = message + \
             f"Permitted models: {self.permitted_models}"
@@ -24,31 +33,45 @@ class GasNotRecognizedException(Exception):
     """Exception raised if attempting to select gas of unknown name/type.
 
     Attributes:
-        permitted_gases: tuple with names of recognized emission gases.
-        message: explanation of the error
+        permitted_gases (Tuple[str, ...]): Names of recognized emission gases.
+        message (str): Explanation of the error.
     """
     def __init__(
             self,
             permitted_gases: Tuple[str, ...],
             message: str = "Gas not recognized."):
+        """
+        Initializes the GasNotRecognizedException with permitted gases and an optional message.
+
+        Args:
+            permitted_gases (Tuple[str, ...]): Permitted emission gases.
+            message (str, optional): Additional message to include in the exception. Defaults to "Gas not recognized.".
+        """
         self.permitted_gases = permitted_gases
         self.message = message + f"Permitted gases: {self.permitted_gases}"
         super().__init__(self.message)
 
 
 class WrongAreaFractionsException(Exception):
-    """Exception raised if the number of area fractions does not match
-    the number of land uses.
+    """Exception raised if the number of area fractions does not match the number of land uses.
 
     Attributes:
-        number_of_fractions: number of area fractions in a list
-        number_of_landuses: number of pre-define land uses.
+        number_of_fractions (int): Number of area fractions in a list.
+        number_of_landuses (int): Number of pre-defined land uses.
     """
     def __init__(
             self,
             number_of_fractions: int,
             number_of_landuses: int,
             message: str = ""):
+        """
+        Initializes the WrongAreaFractionsException with the number of fractions and land uses.
+
+        Args:
+            number_of_fractions (int): Number of area fractions.
+            number_of_landuses (int): Number of land uses.
+            message (str, optional): Additional message to include in the exception. Defaults to "".
+        """
         self.message = f"Number of area fractions: {number_of_fractions} " + \
             f"not equal to the number of land uses: {number_of_landuses}."
         super().__init__(" ".join([message, self.message]))
@@ -58,22 +81,36 @@ class WrongSumOfAreasException(Exception):
     """Exception raised if area fractions in a supplied list do not sum to 1.
 
     Attributes:
-        fractions: list of area fractions
-        accuracy: acceptable error in the sum of area fractions.
+        fractions (list): List of area fractions.
+        accuracy (float): Acceptable error in the sum of area fractions.
     """
     def __init__(self, fractions: list, accuracy: float, message: str = ""):
+        """
+        Initializes the WrongSumOfAreasException with the fractions and accuracy.
+
+        Args:
+            fractions (list): List of area fractions.
+            accuracy (float): Acceptable error in the sum of area fractions.
+            message (str, optional): Additional message to include in the exception. Defaults to "".
+        """
         self.message = f"Fractions sum up to: {sum(fractions)} " + \
             f"and are not within 1 +/-: {accuracy}."
         super().__init__(" ".join([message, self.message]))
 
 
 class TableNotReadException(Exception):
-    """Exception raised if a configuration table not found.
+    """Exception raised if a configuration table is not found.
 
-    Atrributes:
-        table: name of the table or list of names of tables that are not found.
+    Attributes:
+        table (Union[str, list]): Name of the table or list of names of tables that are not found.
     """
     def __init__(self, table: Union[str, list]):
+        """
+        Initializes the TableNotReadException with the table name or list of table names.
+
+        Args:
+            table (Union[str, list]): The table(s) that could not be read.
+        """
         if isinstance(table, str):
             self.message = f"Table: {table} could not be read."
         elif isinstance(table, list):
@@ -84,30 +121,32 @@ class TableNotReadException(Exception):
 
 
 class ConversionMethodUnknownException(Exception):
-    """Exception raised if a conversion of an object was not possible because
-    the conversion method supplied is unknown.
+    """Exception raised if a conversion of an object was not possible because the conversion method supplied is unknown.
 
-    Atrributes:
-        conversion_method: name of the conversion method
+    Attributes:
+        conversion_method (str): Name of the conversion method.
     """
     def __init__(self, conversion_method: str, available_methods: Iterable[str]):
+        """
+        Initializes the ConversionMethodUnknownException with the conversion method and available methods.
+
+        Args:
+            conversion_method (str): The conversion method that was not recognized.
+            available_methods (Iterable[str]): A list of available conversion methods.
+        """
         self.message = f"Conversion method {conversion_method} not recognized."
         available_methods_str = ", ".join(available_methods)
         self.message += f"\nAvailable methods: {available_methods_str}"
         super().__init__(self.message)
 
 
-def append_message(exc: Exception, message: str):
+def append_message(exc: Exception, message: str) -> None:
     """
-    Appends `message` to the message text of the exception `e`.
+    Appends a message to the message text of the given exception.
 
-    Parameters
-    ----------
-    exc: Exception
-        An exception instance whose `args` will be modified to include `message`.
-
-    message: str
-        The string to append to the message.
+    Args:
+        exc (Exception): The exception instance whose message will be appended.
+        message (str): The string to append to the exception's message.
     """
     # Write message to arguments
     if exc.args:
@@ -123,17 +162,13 @@ def append_message(exc: Exception, message: str):
         exc.message = message
 
 
-def replace_message(exc: Exception, message: str):
+def replace_message(exc: Exception, message: str) -> None:
     """
-    Replaces the exception message with `message`.
+    Replaces the exception message with the given message.
 
-    Parameters
-    ----------
-    exc: Exception
-        An exception instance whose `args` will be modified to be `message`.
-
-    message: str
-        The string to replace the exception message with.
+    Args:
+        exc (Exception): The exception instance whose message will be replaced.
+        message (str): The string to replace the exception's message with.
     """
     if exc.args:
         exc.args = (message,) + exc.args[1:]
